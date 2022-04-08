98cfcacfe059f67811b747c7b0a80d7a11e82a29
Gabriel Oliveira <gabriel.oliveira@axented.com>
2022-04-08 16:37:29 -0500

    Refactor views

--- app/controllers/tops_controller.rb
+++ app/controllers/tops_controller.rb
@@ -1,8 +1,13 @@
 # frozen_string_literal: true
+ITEMS_PER_PAGE ||= 30
 
 class TopsController < ApplicationController
   def show
-    top_stories_json = JSON.parse HTTP.get('https://hacker-news.firebaseio.com/v0/topstories.json?print=pretty').to_s
-    @stories = top_stories_json[0..29]
+    @stories = TopItem.order(:location)
+                      .limit(ITEMS_PER_PAGE)
+                      .includes(:item)
+  end
+  def create
+    LoadTopItemsJob.perform_now
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -3,4 +3,5 @@
 Rails.application.routes.draw do
   root "tops#show"
   resources :items
+  resources :tops
 end
