bb0cc9a9b25d6d128c6b55d68adaa37ddd30647e
Gabriel Oliveira <gabriel.oliveira@axented.com>
2022-04-08 14:19:16 -0500

    Fix stimulus and add items + top views for HN index

---
+++ app/controllers/items_controller.rb
@@ -0,0 +1,8 @@
+# frozen_string_literal: true
+
+class ItemsController < ApplicationController
+  def show
+    @hn_story_id = params[:id]
+    @item = JSON.parse HTTP.get("https://hacker-news.firebaseio.com/v0/item/#{@hn_story_id}.json?print=pretty").to_s
+  end
+end
---
+++ app/controllers/tops_controller.rb
@@ -0,0 +1,8 @@
+# frozen_string_literal: true
+
+class TopsController < ApplicationController
+  def show
+    top_stories_json = JSON.parse HTTP.get('https://hacker-news.firebaseio.com/v0/topstories.json?print=pretty').to_s
+    @stories = top_stories_json[0..29]
+  end
+end
